8b4c66969371bcce3228b5f8d570c3b4dd893769
Crowdbotics <team@crowdbotics.com>
2024-06-12 19:23:42 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wild-waterfall-48387",
+  url: "https://wild-waterfall-48387.botics.co",
+  api: "https://wild-waterfall-48387.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
